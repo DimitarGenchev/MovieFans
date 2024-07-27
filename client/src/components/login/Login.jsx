444dc76960bab2ef59120a233fab200edd901c5e
DimitarGenchev <d.genchev21@gmail.com>
2024-07-27 23:05:06 +0300

    Implemented login functionality

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,21 +1,38 @@
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/esm/Container';
 import Form from 'react-bootstrap/Form';
+import useForm from '../../hooks/useForm';
+import useLogin from '../../hooks/auth/useLogin';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
+    const login = useLogin();
+    const navigate = useNavigate();
+    const [formValues, changeHandler, submitHandler] = useForm(
+        { email: '', password: '' },
+        async ({ email, password }) => {
+            try {
+                await login(email, password);
+                navigate('/movies');
+            } catch (error) {
+                console.log(error.message);
+            }
+        }
+    );
+
     return (
         <Container style={{ maxWidth: '600px' }}>
             <h1 className="mb-4">Login</h1>
 
-            <Form>
+            <Form onSubmit={submitHandler}>
                 <Form.Group className="mb-3">
                     <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" name="email" />
+                    <Form.Control type="email" name="email" value={formValues.email} onChange={changeHandler} />
                 </Form.Group>
 
                 <Form.Group className="mb-3">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" name="password" />
+                    <Form.Control type="password" name="password" value={formValues.password} onChange={changeHandler} />
                 </Form.Group>
 
                 <Button variant="primary" type="submit">
