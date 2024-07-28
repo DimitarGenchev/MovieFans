9a15e48c917d102f4fec7302f2a062283fef20ad
DimitarGenchev <d.genchev21@gmail.com>
2024-07-28 19:11:51 +0300

    Added dynamic navigation, fixed password saving in state

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,9 +1,13 @@
+import { useContext } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthContext';
 
 export default function Header() {
+    const { isAuthenticated } = useContext(AuthContext);
+
     return (
         <header className='mb-5'>
             <Navbar bg="dark" data-bs-theme="dark">
@@ -11,11 +15,21 @@ export default function Header() {
                     <Navbar.Brand as={Link} to="/">MovieFans</Navbar.Brand>
                     <Nav className="me-auto">
                         <Nav.Link as={Link} to="/movies">All movies</Nav.Link>
-                        <Nav.Link as={Link} to="/movies/create">Create movie</Nav.Link>
-                        <Nav.Link as={Link} to="/login">Login</Nav.Link>
-                        <Nav.Link as={Link} to="/register">Register</Nav.Link>
-                        <Nav.Link as={Link} to="#pricing">Logout</Nav.Link>
-                        <Nav.Link as={Link} to="#pricing">My movies</Nav.Link>
+                        {isAuthenticated
+                            ? (
+                                <>
+                                    <Nav.Link as={Link} to="/movies/create">Create movie</Nav.Link>
+                                    <Nav.Link as={Link} to="#pricing">My movies</Nav.Link>
+                                    <Nav.Link as={Link} to="#pricing">Logout</Nav.Link>
+                                </>
+                            )
+                            : (
+                                <>
+                                    <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                                    <Nav.Link as={Link} to="/register">Register</Nav.Link>
+                                </>
+                            )
+                        }
                     </Nav>
                 </Container>
             </Navbar>
--- client/src/hooks/auth/useRegister.js
+++ client/src/hooks/auth/useRegister.js
@@ -6,9 +6,9 @@ export const useRegister = () => {
     const { changeAuthState } = useContext(AuthContext);
 
     const registerHandler = async (email, password) => {
-        const result = await register(email, password);
+        const { password: _, ...authData } = await register(email, password);
 
-        changeAuthState(result);
+        changeAuthState(authData);
 
         return result;
     };
